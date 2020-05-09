905c4c643ad480dd00dc33385ff6ba9d0552527f
yournian_mac <yournian@gmail.com>
2020-05-10 00:13:19 +0800

    间隔启动更新rss

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-// global.lastUpdateTime = {
-//     'youtube' : {}
-// };
+const schedule = require('./util/schedule');
 const updateRss = require('./logic/updateRss');
-updateRss();
+
+function update(){
+    updateRss();
+}
+
+// 每隔6小时运行一次
+schedule.repeat('* */6 * * *', update);
+
