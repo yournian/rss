e22310f31a7fd838a9144373433fba7a5275f0b0
yournian_mac <yournian@gmail.com>
2020-05-26 20:02:30 +0800

    优化启动代码

--- podcast.js
+++ podcast.js
@@ -3,15 +3,19 @@ const schedule = require('./util/schedule');
 const Updater = require('./logic/updater');
 const {CHANNEL, PORT} = require('./consts');
 
-global.test = true;
+global.test = false;
+
+function run(){
+    // 即刻运行
+    update();
+    // 每隔6小时运行一次
+    schedule.repeat('01 */6 * * *', update);
+    http.listen(PORT);
+}
 
 function update(){
     let updater = new Updater();
     updater.updateFeeds(CHANNEL);
 }
 
-update();
-// 每隔6小时运行一次
-schedule.repeat('01 */6 * * *', update);
-
-http.listen(PORT);
+run();
