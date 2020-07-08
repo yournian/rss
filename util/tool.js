9c3ba2a93cba8143ffebfb951c3c218679bc70ba
yournian_mac <yournian@gmail.com>
2020-07-08 15:58:03 +0800

    测试

--- util/tool.js
+++ util/tool.js
@@ -2,6 +2,6 @@ const escape_html = require('escape-html');
 
 exports.escape = function(str){
     // return escape_html(str);
-    str = str.replace('&', '&amp;');
+    str = str.replace(/&/g, '&amp;').replace(/\-/g, ':');
     return str;
 }
