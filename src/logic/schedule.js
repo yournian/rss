75daae6c4e8301cee8ea73867fa2343c9c38d7f3
yournian_mac <yournian@gmail.com>
2021-01-29 17:51:24 +0800

    修复字段错误

--- src/logic/schedule.js
+++ src/logic/schedule.js
@@ -5,7 +5,7 @@ const ctx = require('../context');
 async function loadJobs(){
     const models = ctx.models;
     const crontabs = await models.crontabs.findAll({
-        attributes: ['name',['_value', 'value'], 'type', 'encoding', 'interval', 'enable', 'immediate', 'rules', 'description'],
+        attributes: ['name',['_value', 'value'], 'type', 'encoding', 'interval', 'enable', 'immediate', 'rule', 'description'],
         where: {enable: 1}
     });
     let jobMgr = new JobMgr();
