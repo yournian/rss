fe4dd9f3c5b7b8bc4d674bcc9844d133a5d29b93
yournian_mac <yournian@gmail.com>
2021-02-03 15:12:07 +0800

    修改dev环境参数

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     "domain": "https://yournian.top",
-    "port" : 3030,
+    "port" : 3020,
     "youtube_key": "",
     "logLvl": "info",
     "env": 'dev',
     "mysql": {
-        'database': 'rss',
+        'database': 'rss_dev',
         'host': 'localhost',
         'port': 3306,
         'user': 'root',
